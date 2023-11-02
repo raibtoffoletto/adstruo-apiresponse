02b8d150ecd20e9021e824c929176a1b87c469e9
Raí B. Toffoletto <rai@toffoletto.me>
2023-11-02 07:43:42 +0000

    feat: create invalid model handler

---
+++ Adstruo.ApiResponse/ApiInvalidModel.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Adstruo.ApiResponse;
+
+public class ApiInvalidModel : IActionResult
+{
+    private readonly ModelStateDictionary? _model = null;
+
+    public ApiInvalidModel(ModelStateDictionary? model = null)
+    {
+        _model = model;
+    }
+
+    public virtual async Task ExecuteResultAsync(ActionContext context)
+    {
+        HttpResponse res = context.HttpContext.Response;
+
+        ApiResult<ModelStateDictionary?> result = new();
+        result.SetData(_model);
+        result.SetError(new FailedToParseRequestException());
+
+        res.StatusCode = result.Code;
+
+        await res.JsonSerializerAsync(result);
+    }
+}
---
+++ Adstruo.ApiResponse/HttpResponseExtensions.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Adstruo.ApiResponse;
+
+public static class HttpResponseExtensions
+{
+    public static async Task<HttpResponse> JsonSerializerAsync(this HttpResponse res, object? value)
+    {
+        res.ContentType = "application/json; charset=utf-8";
+
+        await res.WriteAsync(
+            JsonConvert.SerializeObject(
+                value,
+                Formatting.None,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new CamelCaseNamingStrategy()
+                    },
+                }
+            )
+        );
+
+        return res;
+    }
+}
